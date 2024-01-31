ce0ef962d95fd5b184df106590158a3f750a8566
JEANFRANCOIS HAFID <hafidjeanfrancois@gmail.com>
2024-01-31 18:20:00 +0100

    FEAT: Initial commit with DB schematic and seeding

---
+++ server/bin/customSeed.js
@@ -0,0 +1,112 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+
+// Load environment variables from .env file
+require("dotenv").config();
+
+// Import Faker library for generating fake data
+const { fakerFR, faker } = require("@faker-js/faker");
+
+// Import database client
+const database = require("../database/client");
+
+const seed = async () => {
+  try {
+    // Declare an array to store the query promises
+    // See why here: https://eslint.org/docs/latest/rules/no-await-in-loop
+    const queries = [];
+
+    /* ************************************************************************* */
+
+    // Customs variables for seeding
+    const password =
+      "$argon2id$v=19$m=19456,t=2,p=1$fOujflfWPGrhA1QJivUYwQ$cKlZzL5vBmyGkMYAALYpxZtksjqhtpuDOX4uCUCmHqc";
+
+    const typeOffer = ["sell", "rent"];
+    // const coordinates = `POINT(48.873787, 2.295047)`;
+    // { x: fakerFR.location.latitude(), y: fakerFR.location.longitude() },
+
+    // Insert fake data with a loop
+    for (let i = 0; i < 3; i += 1) {
+      queries.push(
+        database.query(
+          "INSERT INTO agency (name, email, address, coordinates, city, country, contact_number) VALUES (?, ?, ?, ?, ?, ?, ?)",
+          [
+            fakerFR.company.name(),
+            fakerFR.internet.email(),
+            fakerFR.location.streetAddress(),
+            null,
+            fakerFR.location.city(),
+            faker.location.country(),
+            fakerFR.helpers.fromRegExp(/[0]{1}[1-9]{1}[0-9]{8}/),
+          ]
+        ),
+        database.query(
+          `UPDATE agency SET coordinates = POINT(?, ?)`,
+          [48.873787, 2.295047]
+        ),
+        database.query(
+          "INSERT INTO user (lastname, firstname, email, password, contact_number) VALUES (?, ?, ?, ?, ?)",
+          [
+            fakerFR.person.lastName(),
+            fakerFR.person.firstName(),
+            fakerFR.internet.email(),
+            password,
+            fakerFR.helpers.fromRegExp(/[0]{1}[1-9]{1}[0-9]{8}/),
+          ]
+        )
+      );
+    }
+    for (let i = 0; i < 3; i += 1) {
+      queries.push(
+        database.query(
+          "INSERT INTO commercial (lastname, firstname, email, password, avatar, agency_id, contact_number) VALUES (?, ?, ?, ?, ?, ?, ?)",
+          [
+            fakerFR.person.lastName(),
+            fakerFR.person.firstName(),
+            fakerFR.internet.email(),
+            password,
+            fakerFR.internet.avatar(),
+            fakerFR.number.int({ min: 1, max: 3 }),
+            fakerFR.helpers.fromRegExp(/[0]{1}[1-9]{1}[0-9]{8}/),
+          ]
+        )
+      );
+    }
+    for (let i = 0; i < 50; i += 1) {
+      queries.push(
+        database.query(
+          "INSERT INTO offer (title, description, size, coordinates, city, country, type, price, commercial_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          [
+            faker.lorem.sentence({ min: 3, max: 6 }),
+            faker.lorem.paragraph({ min: 3, max: 10 }),
+            fakerFR.number.int({ min: 20, max: 500 }),
+            null,
+            fakerFR.location.city(),
+            faker.location.country(),
+            faker.helpers.arrayElement(typeOffer, 1),
+            fakerFR.number.int({ min: 500, max: 5000000 }),
+            fakerFR.number.int({ min: 1, max: 3 }),
+          ]
+        ),
+        database.query(
+          `UPDATE offer SET coordinates = POINT(?, ?)`,
+          [48.873787, 2.295047]
+        )
+      );
+    }
+    /* ************************************************************************* */
+
+    // Wait for all the insertion queries to complete
+    await Promise.all(queries);
+
+    // Close the database connection
+    database.end();
+
+    console.info(`${database.databaseName} filled from ${__filename} 🌱`);
+  } catch (err) {
+    console.error("Error filling the database:", err.message);
+  }
+};
+
+// Run the seed function
+seed();
--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,51 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+CREATE TABLE `agency` (
+  id INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(200) NOT NULL,
+  `address` VARCHAR(255) NOT NULL,
+  `coordinates` POINT NULL,
+  `city` VARCHAR(100) NOT NULL,
+  `country` VARCHAR(100) NOT NULL,
+  `contact_number` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+
+CREATE TABLE `user` (
+  id INT NOT NULL AUTO_INCREMENT,
+  `lastname` VARCHAR(100) NOT NULL,
+  `firstname` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(255) UNIQUE NOT NULL,
+  `password` VARCHAR(200) NOT NULL,
+  `contact_number` VARCHAR(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `commercial` (
+  id INT NOT NULL AUTO_INCREMENT,
+  `lastname` VARCHAR(100) NOT NULL,
+  `firstname` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(200) UNIQUE NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `avatar` VARCHAR(255) NOT NULL,
+  `agency_id` INT NOT NULL,
+  `contact_number` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `commercial_agency_fk` FOREIGN KEY (`agency_id`) REFERENCES `agency`(`id`)
+);
+
+CREATE TABLE `offer` (
+  id INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `description` text NOT NULL,
+  `size` INT UNSIGNED NOT NULL,
+  `coordinates` POINT NULL,
+  `city` VARCHAR(200) NOT NULL,
+  `country` VARCHAR(100) NOT NULL,
+  `type` VARCHAR(4) NOT NULL,
+  `creation_date` DATETIME NOT NULL DEFAULT NOW(),
+  `price` INT UNSIGNED NOT NULL,
+  `commercial_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `offer_commercial_fk` FOREIGN KEY (`commercial_id`) REFERENCES `commercial`(`id`)
 );
